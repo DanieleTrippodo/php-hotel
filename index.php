6bc01a7c4be8608bda2bfe720339739c67dcc124
DanieleTrippodo <trippododaniele@gmail.com>
2024-06-27 16:49:41 +0200

    divide component inside index and removes main.js

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel PHP</title>
+
+    <!-- Collegamento Bootstrap -->
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+
 </head>
 
 
@@ -11,46 +15,17 @@
 
 
 <body>
-    <h1>Elenco</h1>
+
+
+
+    <h1 class="my-4">Elenco</h1>
+
+    <!-- Collegamento alla nostra array fornita -->
+    <?php  require_once __DIR__ . "/src/hotels.php"   ?>
+
+
 
     <?php
-    $hotels = [
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-    ];
 
     foreach ($hotels as $hotel) {
         echo "<h2>{$hotel['name']}</h2>";
